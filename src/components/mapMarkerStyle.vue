3611c05da5e59240dbf712faede6fca1cd880b5a
huang <h_peifen@163.com>
2019-08-29 17:36:25 +0800

    自定义标记点

--- src/components/mapMarkerStyle.vue
+++ src/components/mapMarkerStyle.vue
@@ -3,11 +3,12 @@
     <div id="myMap" class="map-box"></div>
         <div class="legend-container">
             <p class="legend-title">图例</p>
-            <div class="legend-content" >
+            <div>
                 <p v-for="(item, index) in pointCheck" :key="index">
-                    <input name="Fruit" type="checkbox" :label="item.name" @click="selectIcon($event)"/>
-                    <i :class="'iconfont '+item.icon"></i>{{item.name}}
+                    <input name="Fruit" type="checkbox" v-model="pointSelect" :value="item.value" :label="item.name"/>
+                    <i class="legend-icon iconfont" :class="item.icon"></i>{{item.name}}
                 </p> 
+                <span class="legend-tips">灰色为离线装置</span>
             </div>
         </div>
     </div>
@@ -22,7 +23,8 @@ export default {
     return {
       mapKey:'', //你的key
       marker: [],
-      mapData: [
+      // 初始的标记点，由后台返回（有后台接口时不需要设置）
+      mapOriginData: [
         {name:'标记1',tips:'这是标记1',lat:30.16, lng:120.53,type:'warning',status:-1},
         {name:'标记2',tips:'这是标记2',lat:30.40, lng:120.26,type:'warning',status:0},
         {name:'标记3',tips:'这是标记3',lat:30.04, lng:120.41,type:'warning',status:0},
@@ -30,16 +32,34 @@ export default {
         {name:'标记5',tips:'这是标记5',lat:30.18, lng:120.45,type:'watch',status:0},
         {name:'标记6',tips:'这是标记6',lat:30.46, lng:120.24,type:'watch',status:0},
       ],
-      pointSelect: ['预警点','监测点'],
+      mapData:[],
+      pointSelect: ['warning','watch'],
       pointCheck: [
-          {icon:'icon-tufashijian',name:'预警点'},
-          {icon:'icon-shexiangtou',name:'监测点'},
-      ]
+          {icon:'icon-tufashijian',name:'预警点1',value:'warning',checked:true},
+          {icon:'icon-tufashijian1',name:'预警点2',value:'watch',checked: true},
+      ],
+      map: null
     }
   },
   mounted(){
+      this.mapData = this.mapOriginData
       Provider(L) // 挂载插件
       this.getMap() 
+  },
+  watch:{
+    pointSelect(val){
+        let data = []
+        // 这里是静态数据模拟，实际项目里由后台传值过来
+        val.map(res=>{
+            this.mapOriginData.map(val=>{
+                if(res == val.type){
+                    data.push(val)
+                }
+            })
+        })
+        this.mapData = data
+        this.getPointer()
+    }
   },
   methods:{
       getMap(){
@@ -50,10 +70,12 @@ export default {
         })
         L.tileLayer.chinaProvider('TianDiTu.Normal.Map',{maxZoom:18,minZoom:5,key:this.mapKey}).addTo(map);
         L.tileLayer.chinaProvider('TianDiTu.Normal.Annotion',{maxZoom:18,minZoom:5,key:this.mapKey}).addTo(map);
-        this.getPointer(map)
+        this.map = map
+        this.getPointer()
     },
-    getPointer(map){
+    getPointer(){
         let _this = this // 防止冲突
+        let map = _this.map
         let htmlPoint = ''
         let pointIcon = ''
         if(this.marker){
@@ -63,13 +85,21 @@ export default {
         }
         this.mapData.map((res,index)=>{
             pointIcon = _this.getIcon(res.type,res.status)//返回图标的配置
-            htmlPoint = `<span class="icon-bg"><i class="iconfont ${pointIcon.status} ${pointIcon.icon}"></i></span>` // 使用css统一修改颜色
+            htmlPoint = `<span class="icon-bg"><i class="iconfont icon-map ${pointIcon.icon} ${pointIcon.status}"></i></span>` // 使用css统一修改颜色
             let marker = L.marker([res.lat,res.lng]).addTo(map);
             map.addLayer(marker) // 添加标记
-            marker.setIcon(L.divIcon({   // 标记配置-详见leaflet官网
+            // 标记配置-详见leaflet官网
+            marker.setIcon(L.divIcon({
                 html: htmlPoint,
-                className: `point-tips` // 设置marker的class名
+                // className: `` // 可以自定义marker的class名
             }))
+
+            // 添加鼠标悬浮事件
+            marker.bindTooltip(res.name, {
+                direction: 'right' ,
+                className: `tool-div ${pointIcon.icon} ${pointIcon.status}`,
+                offset: [-20, -10] 
+            })
             
             // 自定义其他事件
             marker.on('click', ()=>{
@@ -80,11 +110,11 @@ export default {
     },
     getIcon(type,status){
         let pointIcon = {}
-        // 根据点击切换图标
+        // 根据类型切换图标
         if(type == 'warning'){
             pointIcon.icon = 'icon-tufashijian'
         }else{
-            pointIcon.icon = 'icon-shexiangtou'
+            pointIcon.icon = 'icon-tufashijian1'
         }
         if(status == -1){
             // 状态-1为离线
@@ -93,21 +123,16 @@ export default {
             pointIcon.status = ''
         }
         return pointIcon
-    },
-    selectIcon(e){
-        console.log(e,'eee');
-        
-
     }
   }
 }
 </script>
-<style scoped>
+<style>
 .icon-tufashijian{
     color:#FF4931;
     background:#FF4931;
 }
-.icon-shexiangtou{
+.icon-tufashijian1{
     color:#4CCB64;
     background:#4CCB64;
 }
@@ -115,43 +140,55 @@ export default {
     color: gray;
     background:gray;
 }
-
-.point-tips{
-    width: 0.36rem!important;
-    height: 0.36rem!important;
-    line-height: 0.36rem;
+.iconfont{
+    font-size: 30px;
+    background: none;
+}
+.legend-icon{
+    font-size: 18px;
 }
 .icon-bg{
+    /*自定位图表的位置会有点便宜，需要设置位置*/
     background: #fff;
     border-radius: 50%;
     position: relative;
-    left: -0.1rem;
-    top: -0.2rem;
-    width: 0.3rem;
-    height: 0.3rem;
+    left: -8px;
+    top: -16px;
+    width: 20px;
+    height: 20px;
     text-align: center;
     display: inline-block;
 }
-.icon-normal{
-    font-size: 0.36rem;
+.icon-map{
     position: relative;
-    left: -0.03rem;
+    left: -3px;
+    top: -8px;
     background: none;
 }
 .tool-div{
     color: #fff;
-    /* background: #2A5D96; */
     border: none;
-    height: 0.34rem;
-    line-height: 0.38rem;
-    padding: 0 0.15rem 0 0.36rem;
-    border-radius: 0.3rem;
+    height: 25px;
+    line-height: 25px;
+    padding: 0 15px 0 36px;
+    border-radius: 30px;
 }
-
+.leaflet-tooltip-pane{
+    z-index: 200;
+}
+/* 将原来的定位点隐藏 */
+.leaflet-marker-icon{
+    width: 0!important;
+    height: 0!important;
+}
+/* tooltip左侧的小箭头 */
+.leaflet-tooltip-right:before{
+    opacity: 0;
+}
 .legend-container{
     position: relative;
     z-index: 650;
-    top: -150px;
+    top: -160px;
     left: 0.31px;
     background: rgba(252, 252, 251, 0.92);
     width: 170px;
@@ -167,8 +204,10 @@ export default {
     padding-bottom: 10px;
     border-bottom: 1px solid #ccc;
 }
-.iconfont{
-    font-size: 18px;
-    background: none;
+.legend-tips{
+    font-size: 14px;
+    color: #666;
 }
+
+
 </style>
